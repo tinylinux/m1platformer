0a045db7fcbc870140379f1a0962014a665bdfd0
Rida Lali <ridalal23@hotmail.fr>
2020-10-16 15:50:56 +0200

    Add quit event

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import random, time
 
 pygame.init()
 
-FPS = 40
+FPS = 60
+FramePerSec = pygame.time.Clock()
 
 # Display configuration for user
 ch, cw = pygame.display.Info().current_h, pygame.display.Info().current_w
@@ -15,3 +16,12 @@ SCREEN_HEIGHT = min((ch * 8)//9, 9*SCREEN_WIDTH//16)
 ecran = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Platformer")
+
+while True:
+    # print('OK : ', pygame.time.get_ticks())
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    FramePerSec.tick(FPS)
