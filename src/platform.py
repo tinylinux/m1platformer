21b79bc51dd1133f1441f11ed44d34eafc8ecaf8
Rida Lali <ridalal23@hotmail.fr>
2020-10-16 18:36:04 +0200

    Infinite platforms fixed

--- src/platform.py
+++ src/platform.py
@@ -11,12 +11,14 @@ class plateforme(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect(topleft = (x,y))
         pygame.sprite.Sprite.__init__(self, sol)
         #Ajoute notre plateforme au groupe "sol". sol.sprites() est la liste des plateformes
+        self.pasencorecree = True
 
     def move(self):
         self.rect.move_ip(-SPEED,0)
         #Défile à la vitesse SPEED (positive, donc on met un - pour aller à gauche)
         if self.rect.right < 0:     #si on est sorti de l'écran
-            self.kill()               #on le supprime
-        if SCREEN_WIDTH - self.long < self.rect.right < SCREEN_WIDTH:
+            self.kill()              #on le supprime
+        if SCREEN_WIDTH - self.long < self.rect.right < SCREEN_WIDTH and self.pasencorecree:
             #si le dernier ne couvre plus tout sur la droite, il faut ajouter un nouveau
-            plateforme(self.rect.right)    #On en met un nouveau juste après
+            plateforme(self.rect.right)
+            self.pasencorecree = False    #On en met un nouveau juste après
