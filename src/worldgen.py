d9f4ab6c75926964c90fa0f5d17cd3656b04a815
Matteo Chancerel <matteo@macbook-pro-de-matteo.home>
2020-11-11 09:20:18 +0100

    crée les nuages

--- src/worldgen.py
+++ src/worldgen.py
@@ -1,22 +1,32 @@
 """ Gère la génération du monde """
-import pygame
+import pygame, random
 # Import classes
-import src.platform as pltfrm
+import src.platform as plt
 import src.conf as cf
+import src.background as bg
 
 
-def initgen(longueur_totale):
-    """ Lance la création du sol """
+def initgen():
+    #Lance la création du sol
+    
     # on rajoute des bouts de sol, on additionne leur longueur
     # et quand on a couvert tout l'écran on s'arrête.
+    longueur_totale=0
     while longueur_totale < cf.SCREEN_WIDTH:
         # On en met un nouveau à la position x = longueur_totale.
-        pltfrm.Platform(longueur_totale)
+        plt.Platform(longueur_totale)
         longueur_totale += cf.SOL_LONG
+    #Crée quelques nuages
+    for k in range(4) :
+        x = random.randint(0,cf.SCREEN_WIDTH)
+        y = random.randint(0,cf.SCREEN_HEIGHT//2)
+        i = random.randint(0,3)
+        bg.Nuage(x,y,i)
 
-
-def update_sol():
-    """ Met le sol à jour """
+def update():
+    for nuage in cf.nuages :
+        nuage.update()
     for bloc in cf.sol:
-        bloc.move()     # On déplace chaque bloc
-        cf.DISPLAYSURF.blit(bloc.image, bloc.rect)
+        bloc.update()     # On déplace chaque bloc
+
+        
