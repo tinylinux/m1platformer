222be2df1a24251ef3375fc9f418b2ec623e6cdf
Samuel Arsac <samuel.arsac@ens-paris-saclay.fr>
2020-11-13 15:27:35 +0100

    Modification des classes pour les boutons : ajout des images

--- src/menu.py
+++ src/menu.py
@@ -5,39 +5,59 @@ import src.conf as cf
 
 
 white = (255,255,255) 
-light = (170,170,170) 
-dark = (100,100,100)
+idle = (170,170,170) 
+hover = (100,100,100)
 smallfont = pygame.font.SysFont(None,35)
 
 def mouse_on_button(mouse, button_pos, button_size):
     return(int(button_pos[0] - button_size[0]/2) <= mouse[0] <= int(button_pos[0] + button_size[0]/2)\
         and int(button_pos[1] - button_size[1]/2) <= mouse[1] <= int(button_pos[1] + button_size[1]/2))
 
-class Button():
+class Button:
     """ Classe des boutons pour les menus"""
-    def __init__(self, position, size, text):
+    def __init__(self, position, size):
         self.position = position
         self.size = size
-        self.text = text
         self.rect = pygame.Rect([int(position[0] - size[0]/2), int(position[1] - size[1]/2), size[0], size[1]])
-        self.text_position = (int(position[0] - size[0]/2) + 10, int(position[1] - size[1]/2) + 10)
-    
-    def print(self, mouse):
-        """Affiche le bouton"""
-        if mouse_on_button(mouse, self.position, self.size):
-            pygame.draw.rect(cf.DISPLAYSURF, light, self.rect)
-        else:
-            pygame.draw.rect(cf.DISPLAYSURF, dark, self.rect)
-        cf.DISPLAYSURF.blit(self.text, self.text_position)
 
     def click(self, mouse):
         """Renvoie si la souris est sur le bouton"""
         return(mouse_on_button(mouse, self.position, self.size))
 
-start_button = Button((int(1280/2), int(720/2)),\
+class Button_text(Button):
+    """Classe des boutons affichant du texte"""
+    def __init__(self, position, size, text):
+        super().__init__(position, size)
+        self.text = text
+        self.text_position = (int(position[0] - size[0]/2) + 10, int(position[1] - size[1]/2) + 10)
+    
+    def print(self, mouse):
+        """Affiche le bouton"""
+        if mouse_on_button(mouse, self.position, self.size):
+            pygame.draw.rect(cf.DISPLAYSURF, hover, self.rect)
+        else:
+            pygame.draw.rect(cf.DISPLAYSURF, idle, self.rect)
+        cf.DISPLAYSURF.blit(self.text, self.text_position)
+
+class Button_image(Button):
+    """Classe des boutons affichant une image"""
+    def __init__(self, position, size, image, image_hover):
+        super().__init__(position, size)
+        self.image = image
+        self.image_hover = image_hover
+    
+    def print(self, mouse):
+        """Affiche le bouton"""
+        if mouse_on_button(mouse, self.position, self.size):
+            cf.DISPLAYSURF.blit(self.image_hover, self.position)
+        else:
+            cf.DISPLAYSURF.blit(self.image, self.position)
+
+
+start_button = Button_text((int(1280/2), int(720/2)),\
     (160, 40),\
     smallfont.render('Commencer' , True , white))
 
-restart_button = Button((int(1280/2), int(720/2)),\
+restart_button = Button_text((int(1280/2), int(720/2)),\
     (185, 40),\
     smallfont.render('Recommencer' , True , white))
