8cec192b9fb5b0d5a2188f20c9ad0986ae37e353
Rida Lali <ridalal23@hotmail.fr>
2020-11-19 22:03:23 +0100

    EasterEgg pour vous :egg:

--- src/score.py
+++ src/score.py
@@ -44,11 +44,22 @@ def get_best_score():
     with open(FILE) as board:
         try:
             int(board.read().strip().replace("\n", ""))
-        except:
-            board.close()
-            init_best_score()
+        except Exception as error_of_type:
+            if error_of_type = ValueError:
+                board.close()
+                init_best_score()
+            else:
+                ok()
         else:
             return int(board.read().strip().replace("\n", ""))
+
+
+def ok():
+    """
+    Bravo vous avez réussi à avoir un EasterEgg
+    """
+    print("On t'a vu petit malin !")
+    pygame.quit()
 
 
 def set_best_score(value):
