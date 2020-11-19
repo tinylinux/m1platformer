892de059ff5c318e35d0cd0f13a7cea9d829f095
Samuel Arsac <samuel.arsac@ens-paris-saclay.fr>
2020-11-19 23:55:55 +0100

    Gestion de multiples scores avec noms

--- src/score.py
+++ src/score.py
@@ -37,17 +37,39 @@ def score_endgame(pts):
                   pygame.font.Font(mn.FONT_PIXEL, 50), True)
 
 
-def get_best_score():
+def get_scores():
     """
     Récuperer le score sauvegardé dans le scoreboard
     """
     with open(FILE) as board:
         try:
-            return int(board.read().strip().replace("\n", ""))
+            scores = board.readlines()
+            if len(scores) < 2:
+                return [[], []]
+            scores[0].split(";")
+            scores[1].split(";")
+            scores[1] = map(int, scores[1])
+            ordered_val = list(reversed(sorted(scores[1])))
+            ordered_i = []
+            for i in range(len(scores[0])):
+                ordered_i.append(ordered_val.index(scores[1][i]))
+            ordered_names = []
+            for i in range(len(scores[0])):
+                ordered_names.append(scores[0][ordered_i[i]])
+            return [ordered_names, ordered_val]
         except ValueError:
             board.close()
             init_best_score()
-            return 0
+            return [[], []]
+
+
+def get_last_best_score():
+    """Renvoie le plus petit score du leaderboard"""
+    scores = get_scores()
+    if len(scores[0]) == 0:
+        return 0
+    last = min(scores[1])
+    return last
 
 
 def set_best_score(value):
@@ -64,7 +86,7 @@ def maj(pts):
     Si oui, il retourne True et modifie le High-Score
     Si non, il retourne False
     """
-    if get_best_score() < pts:
+    if get_last_best_score() < pts:
         set_best_score(pts)
         return True
     return False
