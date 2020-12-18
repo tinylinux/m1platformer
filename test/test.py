2c647c34fef02b60dc3b00e6a938523e771d0715
Matteo Chancerel <matteo@macbook-pro-de-matteo.home>
2020-12-19 00:06:26 +0100

    adapte test.py aux modifs de collide. fix #21

--- test/test.py
+++ test/test.py
@@ -3,12 +3,14 @@
 from hypothesis import given
 from hypothesis.strategies import integers
 import src.sprites as spt
+import src.player as pl
 from src.utilities import Vec
-from src.player import collide
 from src.utilities import create_rect
 
 
 # Test des collisions
+
+player = pl.Player()
 
 # pylint: disable=too-many-arguments
 @given(integers(min_value=0, max_value=100),
@@ -25,12 +27,12 @@ def test_collisions(pos_prev_x, pos_prev_y,
                     width_plat, height_plat):
     """Test pour la fonction de vérification des collisions"""
     dummy_next = create_rect([pos_next_x, pos_next_y,
-                              spt.p_WIDTH, spt.p_HEIGHT])
+                              player.width, player.height])
     plat = create_rect([pos_plat_x, pos_plat_y,
                         width_plat, height_plat])
-    (vert, hor, new_pos) = collide(Vec((pos_prev_x, pos_prev_y)),
-                                   Vec((pos_next_x, pos_next_y)),
-                                   plat)
+    (vert, hor, new_pos) = player.collide(Vec((pos_prev_x, pos_prev_y)),
+                                          Vec((pos_next_x, pos_next_y)),
+                                          plat)
     assert (vert or hor) == dummy_next.colliderect(plat)
     if new_pos is not None:
         dummy_next.topleft = new_pos
