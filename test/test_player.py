ff36e74a05be8dd5cffd996f60285c03379020bc
Samuel Arsac <samuel.arsac@ens-paris-saclay.fr>
2021-01-02 16:57:17 +0100

    Test du saut #27

--- test/test_player.py
+++ test/test_player.py
@@ -1,11 +1,14 @@
-""" Fichier de test """
+"""Fichier de test pour player."""
 
 from hypothesis import given
 from hypothesis.strategies import integers
 import src.sprites as spt
+import src.conf as cf
+from main import initialization
 from src.utilities import Vec
 from src.player import collide, Player
 from src.utilities import create_rect
+from src.gameloop import main_loop
 
 
 # Test des collisions
@@ -23,7 +26,7 @@ def test_collisions(pos_prev_x, pos_prev_y,
                     pos_next_x, pos_next_y,
                     pos_plat_x, pos_plat_y,
                     width_plat, height_plat):
-    """Test pour la fonction de vérification des collisions"""
+    """Test pour la fonction de vérification des collisions."""
     player = Player()
     player.pos = Vec((pos_prev_x, pos_prev_y))
     dummy_next = create_rect([pos_next_x, pos_next_y,
@@ -38,3 +41,22 @@ def test_collisions(pos_prev_x, pos_prev_y,
         dummy_next.topleft = new_pos
     assert not dummy_next.colliderect(plat)
 # pylint: enable=too-many-arguments
+
+
+# Test du saut du joueur
+
+def test_jump():
+    """Test pour la fonction de saut de la classe Player."""
+    cf.NB_PLAYERS = 1
+    _, player = initialization(False)
+    player = player[0]
+    cf.STATE = cf.State.ingame
+    # Lancement du saut
+    player.jump()
+    assert player.vel.y == -cf.V_JMP
+    # Quelques tours de boucle
+    for _ in range(10):
+        main_loop([player], False)
+    # Vitesse finale, avec précision prise en compte
+    assert player.vel.y <= -cf.V_JMP + 10 * cf.G + 0.001\
+        and player.vel.y >= -cf.V_JMP + 10 * cf.G - 0.001
