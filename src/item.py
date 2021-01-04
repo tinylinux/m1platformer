092e64b1273ba7542206da1bfa7a78afdf198640
Samuel Arsac <samuel.arsac@ens-paris-saclay.fr>
2021-01-04 20:52:44 +0100

    Résolution d'erreurs pylint / flake8 #25

--- src/item.py
+++ src/item.py
@@ -1,4 +1,4 @@
-""" Gère la génération d'items"""
+"""Gère la génération d'items."""
 
 import random as rd
 import src.conf as cf
@@ -13,31 +13,38 @@ ITEMS = ["fast", "slow", "big", "little"]
 
 
 class item(ut.GameObject):
-    """Gère les items
-        i = n° d'item :
-            0 = fast
-            1 = slow
-            2 = big
-            3 = little"""
+    """
+    Gère les items.
+
+    i = n° d'item :
+        0 = fast
+        1 = slow
+        2 = big
+        3 = little
+    """
+
     def __init__(self, plt):
-        """Crée un item sur une plateforme
-            plt : plateforme sur laquelle apparait l'item"""
+        """
+        Crée un item sur une plateforme.
+
+        plt : plateforme sur laquelle apparait l'item
+        """
         cf.FLAG_ITEM = True
-        i = rd.randint(0, spt.img_dict["n_item"]-1)
+        i = rd.randint(0, spt.img_dict["n_item"] - 1)
         self.type = ITEMS[i]
         img = spt.img_dict["item_img"][i]
         w, h = img.get_rect().size
 
         x_plt, y_plt = plt.pos
         dx = plt.dim[0]  # longueur de la plateforme
-        x = rd.randint(x_plt, x_plt+dx-w)
-        y = y_plt-h
+        x = rd.randint(x_plt, x_plt + dx - w)
+        y = y_plt - h
 
         super().__init__((x, y), 1, img)
         ut.add_to_group(self, spt.items)
 
     def update(self):
-        """update l'item"""
+        """Update l'item."""
         super().update()
         if self.rect.left < cf.SPEED:  # Si l'item va sortir de l'écran bientôt
             cf.FLAG_ITEM = False     # On annule le cf.FLAG_ITEM
